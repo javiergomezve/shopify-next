9b1b4bb8de5e7de5e7b6c0d7e4dbdc47cbe820b8
Javier Gomez <javiergomezve@gmail.com>
2021-11-30 11:22:49 -0500

    [ADD] Products list

---
+++ components/ProductCard.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { formatter } from '../utils/helpers';
+
+const ProductCard = ({ handle, title, images, priceRange }) => {
+    const { altText, originalSrc } = images.edges[0].node;
+    const { amount: price } = priceRange?.minVariantPrice;
+
+    return (
+        <Link href={`/product/${handle}`}>
+            <a className="group">
+                <div className="w-full overflow-hidden bg-gray-200 rounded-3xl">
+                    <div className="relative group-hover:opacity-75 h-72">
+                        <Image
+                            src={originalSrc}
+                            alt={altText}
+                            layout="fill"
+                            objectFit="cover"
+                        />
+                    </div>
+                </div>
+
+                <h3 className="mt-4 text-lg font-medium text-gray-900">
+                    {title}
+                </h3>
+
+                <p className="mt-1 text-sm text-gray-700">
+                    {formatter.format(price)}
+                </p>
+            </a>
+        </Link>
+    );
+};
+
+export default ProductCard;
